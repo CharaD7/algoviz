3a0f1d16992c56d9bc50757a3101bd92238a6168
Bharat Tiwari <bharat.tiwari@toyota.com>
2022-12-04 00:22:39 -0600

    husky precommit test

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,13 @@
 // as described in docs - https://nextjs.org/docs/basic-features/eslint#lint-staged
 const path = require('path')
 
+// const buildEslintCommand = (filenames) =>
+//   `eslint --quiet --fix --file ${filenames
+//     .map((f) => path.relative(process.cwd(), f))
+//     .join(' --file ')}`
+
 const buildEslintCommand = (filenames) =>
-  `next lint --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`
+  `eslint --quiet --fix ${filenames.join(' ')}`
 
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
---
+++ pages/Visuals/eslintTest4.js
@@ -0,0 +1,2 @@
+const Test = () => <div>visuals</div>;
+export default Test;
