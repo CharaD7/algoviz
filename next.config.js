40940b4ccd1c6483f0a5f63bc6ca20cb209de089
Bharat Tiwari <bharat.tiwari@toyota.com>
2022-12-03 23:52:21 -0600

    update eslint settings

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     eslint: {
-        dirs: ['pages', 'components', 'lib', 'utils'],
+        dirs: ['pages', 'components', 'public', 'lib', 'utils'],
     },
 };
 
