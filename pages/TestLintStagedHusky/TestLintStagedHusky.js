42d14f50a7daceb1418c306a745e781311c9dd3d
Bharat Tiwari <bharat.tiwari@toyota.com>
2022-12-04 19:47:54 -0600

    cleanup temp debug files

--- pages/TestLintStagedHusky/TestLintStagedHusky.js
+++
@@ -1,6 +0,0 @@
-const Test = () => (
-    <div>
-        This file should auto format/correct with Linting and code formatting rules we configured for ESLint and Pretter
-    </div>
-);
-export default Test;
